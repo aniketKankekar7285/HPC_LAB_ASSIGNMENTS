27151c1d4af415163e1546524d9984e89207e439
Aniket Kankekar <76193730+aniketKankekar7285@users.noreply.github.com>
2022-09-27 13:31:23 +0530

    assignment3 and 4 codes updated

---
+++ assignment3/withoutNowait.c
@@ -0,0 +1,20 @@
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#define N 10
+void hello_world() {
+printf("Hello world\n");
+}
+void print(int i) {
+printf("Value %d\n", i);
+}
+int main() {
+#pragma omp parallel
+{
+   
+    for (int i = 0; i < N; i++) {
+      print(i);
+    }
+    hello_world();
+}
+}
